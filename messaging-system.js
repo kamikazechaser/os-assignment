151200b374832f40439662eb029602ffd5d8c9e1
Mohammed Sohail <sohailsameja@gmail.com>
2019-04-22 11:15:19 +0530

    v0.1.0

--- messaging-system.js
+++ messaging-system.js
@@ -2,20 +2,25 @@ const debug = require("debug")("messaging-system");
 const Capq = require("capq");
 const WebSocket = require("ws");
 
-const queue = new Capq({ capacity: 2, autopop: true });
-const wss = new WebSocket.Server({ port: 8080 });
+const queue = new Capq({ capacity: 2, autopop: false });
+const wss = new WebSocket.Server({ port: 80, verifyClient: checkQueue });
 
-wss.on("connection", (socket, req) => {
+function checkQueue(check, done) {
     if (queue.full()) {
         debug("messaging system is full");
-        return socket.send("messaging system is full");
+        return done(false, 401, null, null);
     }
 
+    return done(true);
+}
+
+wss.on("connection", (socket, req) => {
     debug(`${(req.connection.remoteAddress).split(":").pop()} connected to the messaging system`);
     debug(`${queue.capacity() - queue.length()} slots remaining in the messaging system`);
     queue.lfix(req.connection.remoteAddress);
 
     return socket.on("message", msg => {
+        debug(`${(req.connection.remoteAddress).split(":").pop()} => ${msg}`);
         return wss.clients.forEach(function each(client) {
             return client.send(`${(req.connection.remoteAddress).split(":").pop()} => ${msg}`);
         });
